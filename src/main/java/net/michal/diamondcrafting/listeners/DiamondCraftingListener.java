d50a6f916a513fb42ab2966f379b8222a9fd23f0
miszel.ini <michalkolek.pl@gmail.com>
2023-03-11 01:22:54 +0100

    add diamond crafting manager and add some messages to config

--- src/main/java/net/michal/diamondcrafting/listeners/DiamondCraftingListener.java
+++ src/main/java/net/michal/diamondcrafting/listeners/DiamondCraftingListener.java
@@ -2,6 +2,7 @@ package net.michal.diamondcrafting.listeners;
 
 import net.michal.diamondcrafting.WiksaDiamondCrafting;
 import net.michal.diamondcrafting.helpers.ChatHelper;
+import net.michal.diamondcrafting.manager.DiamondCraftingManager;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,14 +14,19 @@ public class DiamondCraftingListener implements Listener {
 
     WiksaDiamondCrafting instance = WiksaDiamondCrafting.getInstance();
 
+    DiamondCraftingManager manager;
 
     @EventHandler
     public void onPlayerCraft(CraftItemEvent event) {
         Player player = (Player) event.getWhoClicked();
-        if(event.getRecipe().getResult().getType().toString().startsWith("DIAMOND_")) {
-            event.setCancelled(true);
-            player.closeInventory();
-            ChatHelper.sendChatMessage(player, instance.getConfig().getString("messages.cannot-crafting"));
+        if(manager.getStatus() == false) {
+            if (event.getRecipe().getResult().getType().toString().startsWith("DIAMOND_")) {
+                event.setCancelled(true);
+                player.closeInventory();
+                ChatHelper.sendChatMessage(player, instance.getConfig().getString("messages.cannot-crafting"));
+            } else {
+                event.setCancelled(false);
+            }
         } else {
             event.setCancelled(false);
         }
@@ -35,8 +41,16 @@ public class DiamondCraftingListener implements Listener {
 
             if(event.getCurrentItem().equals(Material.matchMaterial(instance.getConfig().getString("inventory.on-craft.material")))) {
                 //todo: manager set to enable crafting diamond items
+                if(manager.getStatus() == false) {
+                    manager.setStatus(true);
+                    ChatHelper.sendChatMessage(player, instance.getConfig().getString("messages.enable-crafting"));
+                }
             } else if(event.getCurrentItem().equals(Material.matchMaterial(instance.getConfig().getString("inventory.off-craft.material")))) {
                 //todo: manager set to disable crafting diamond items
+                if(manager.getStatus() == true) {
+                    manager.setStatus(false);
+                    ChatHelper.sendChatMessage(player, instance.getConfig().getString("messages.disable-crafting"));
+                }
             }
         }
     }
---
+++ src/main/java/net/michal/diamondcrafting/manager/DiamondCraftingManager.java
@@ -0,0 +1,14 @@
+package net.michal.diamondcrafting.manager;
+
+public class DiamondCraftingManager {
+
+    static boolean status;
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+}
